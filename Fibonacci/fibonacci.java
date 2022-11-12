fb01fd09e7c6ceef2ec19c9c56f52c2b51ba42dd
Ansari Umer <umer37454@gmail.com>
2022-11-12 19:40:17 +0530

    fibonacci commit

---
+++ Fibonacci/fibonacci.java
@@ -0,0 +1,16 @@
+package practices.Fibonacci;
+
+public class fibonacci {
+    public static void main(String[] args) {
+        int a = 0, b = 1;
+        int c = 0;
+        int n = 10;
+
+        for (int i = 1; i <=n; i ++){
+            System.out.println(c);
+            c = a + b;
+            a = b;
+            b = c;
+        }
+    }
+}
